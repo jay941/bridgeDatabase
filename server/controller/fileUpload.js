b2ff535fd14278ccb48f4ac2d80eaf4efc5e958e
nextitsoftsys <yadav.jayprakash20@yahoo.in>
2016-10-06 10:33:22 +0530

    updated

--- server/controller/fileUpload.js
+++ server/controller/fileUpload.js
@@ -1,30 +1,38 @@
 var express = require('express');
 router = express.Router();
 var jsonfile = require('jsonfile');
+var multiparty = require('multiparty');
 var path1 = './fileUpload/uploadFile.json'
 var upload = require('../helper/multerStorage.js');
 
 
 router.post('/', function(req, res) {
-    console.log("in server" + req.files);
-    upload(req, res, function(err) {
-        if (err) {
-            console.log("error");
-            res.end("Error uploading file.");
-        }
-        console.log("suceess");
-        console.log(req.files);
-        jsonfile.readFile(path1, function(err, obj) {
-            if (err) {
-                console.log("Invalid file");
-            } else {
-                console.log("reading json file");
-                console.log(obj);
-            }
+    console.log(req);
 
-        });
-        res.end("File is uploaded successfully!");
+    var form = new multiparty.Form();
+ 
+    form.parse(req, function(err, fields, files) {
+     console.log('files',files);
     });
+    // console.log("in server" + req.files);
+    // upload(req, res, function(err) {
+    //     if (err) {
+    //         console.log("error");
+    //         res.end("Error uploading file.");
+    //     }
+    //     console.log("suceess");
+    //     console.log(req.files);
+    //     jsonfile.readFile(path1, function(err, obj) {
+    //         if (err) {
+    //             console.log("Invalid file");
+    //         } else {
+    //             console.log("reading json file");
+    //             console.log(obj);
+    //         }
+
+    //     });
+    //     res.end("File is uploaded successfully!");
+    // });
 });
 
 module.exports = router;
